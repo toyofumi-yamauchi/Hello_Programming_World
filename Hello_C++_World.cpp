ada2c0b3b3bea38b3d9051af224b9cddb573b617
Toyo <toyofumi.yamauchi@users.noreply.github.com>
2020-05-11 23:44:33 -0500

    commit

--- Hello_C++_World.cpp
+++ Hello_C++_World.cpp
@@ -4,58 +4,83 @@
 #include <iostream>
 using namespace std;
 
-#define ELEM(array)  (sizeof (array) / sizeof *(array))
-
-struct SStudent
-{
-    char szName[16];
-    int  nJapanese;
-    int  nMath;
-    int  nEnglish;
-};
-
-void Disp(const SStudent& student)
-{
-    cout << "名前 : "   << student.szName    << endl
-         << "　国語 : " << student.nJapanese << " 点, "
-            "数学 : "   << student.nMath     << " 点, "
-            "英語 : "   << student.nEnglish  << " 点" << endl;
-}
-
 int x = 25;  // int型の変数
 int *ptr = &x;  // int型へのポインタ
 
-double y[2];
+double y[3];
+int y0 = 0;
+int size = 3;
 int p;
+
+#define number  100
+struct Test{
+    int     i;
+    double  a;
+    double  *b;
+};
+Test test;
+
+#define m_e 9.1e-31
+#define q   1.6e-19
+#define N_e 200000
+
+struct Particle{
+    double  x;
+    double  v;
+};
+struct Species{
+    double   mass;
+    double   charge;
+    int      N_particle;
+    Particle *part;
+};
+
+int f(int x, int y){
+    return 2 * x + y;
+}
+void g(int x, int y){
+    cout << "g(x,y) = x ^ 2 + y" << endl;
+    cout << "g(" << x << "," << y << ") = " << 2*x+y << "\n" << endl;
+}
 
 int main()
 {
-    cout << "Hello C++ World" << endl;
-    y[0] = 0;
-    for (p = 0; p < 3; p++) {
-        y[p] = p + 1;
-    }
-
-    double *y = &y[3];
-
-    cout << "x = " << x << endl;
-    cout << "&x = " << &x << endl;
+    cout << "Hello C++ World\n" << endl;
+    
+    cout << "x    = " << x << endl;
+    cout << "&x   = " << &x << endl;
     cout << "*ptr = " << *ptr << endl;
-    cout << "ptr = " << ptr << endl;
-    cout << "y[0] = " << y[0] << endl;
-    cout << "y[1] = " << y[1] << endl;
-    cout << "y[2] = " << y[2] << endl;
-    cout << "y = " << *y << endl;
+    cout << "ptr  = " << ptr << "\n" << endl;
 
-    SStudent students[] = {
-        { "赤井孝",   73, 98, 86, },
-        { "笠井大介", 64, 45, 40, },
-        { "吉田叶",   76, 78, 69, },
-    };
-    int      i;
+    y[0] = y0;
+    for (p = 1; p <= size-1; p++) {
+        y[p] = y[p-1] + 1;
+    }
+    double *point = &y[1];
+    cout << "y[0]   = " << y[0] << endl;
+    cout << "y[1]   = " << y[1] << endl;
+    cout << "y[2]   = " << y[size-1] << endl;
+    cout << "*point = " << *point << "\n" << endl;
 
-    for(i = 0; i < ELEM(students); i++)
-        Disp(students[i]);
+    test.i  = number;
+    test.a  = 1;
+    test.b  = new double[test.i];
+    double *b = test.b;
+    cout << "test.i = " << test.i << endl;
+    cout << "test.a = " << test.a << endl;
+    cout << "test.b = " << test.b << endl;
+    cout << "*b = " << *b << "\n" << endl;
 
-    return 0;
+    Species electron;
+
+    electron.mass      =   m_e;
+    electron.charge    =   -q;
+    electron.part      =   new Particle[N_e];
+    cout << "electron.mass = " << electron.mass << endl;
+    cout << "electron.charge = " << electron.charge << endl;
+    cout << "electron.part = " << electron.part << "\n" << endl;
+
+    cout << "f(x,y) = 2 * x + y" << endl;
+    cout << "f(1,2) = " << f(1,2) << endl;
+    g(2,4);
 }
