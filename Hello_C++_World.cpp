deeb9b83b4ae542329ea68fbb223195834a6faa3
Toyo <toyofumi.yamauchi@users.noreply.github.com>
2020-05-11 20:34:17 -0500

    commit

--- Hello_C++_World.cpp
+++ Hello_C++_World.cpp
@@ -4,7 +4,58 @@
 #include <iostream>
 using namespace std;
 
+#define ELEM(array)  (sizeof (array) / sizeof *(array))
+
+struct SStudent
+{
+    char szName[16];
+    int  nJapanese;
+    int  nMath;
+    int  nEnglish;
+};
+
+void Disp(const SStudent& student)
+{
+    cout << "名前 : "   << student.szName    << endl
+         << "　国語 : " << student.nJapanese << " 点, "
+            "数学 : "   << student.nMath     << " 点, "
+            "英語 : "   << student.nEnglish  << " 点" << endl;
+}
+
+int x = 25;  // int型の変数
+int *ptr = &x;  // int型へのポインタ
+
+double y[2];
+int p;
+
 int main()
 {
     cout << "Hello C++ World" << endl;
+    y[0] = 0;
+    for (p = 0; p < 3; p++) {
+        y[p] = p + 1;
+    }
+
+    double *y = &y[3];
+
+    cout << "x = " << x << endl;
+    cout << "&x = " << &x << endl;
+    cout << "*ptr = " << *ptr << endl;
+    cout << "ptr = " << ptr << endl;
+    cout << "y[0] = " << y[0] << endl;
+    cout << "y[1] = " << y[1] << endl;
+    cout << "y[2] = " << y[2] << endl;
+    cout << "y = " << *y << endl;
+
+    SStudent students[] = {
+        { "赤井孝",   73, 98, 86, },
+        { "笠井大介", 64, 45, 40, },
+        { "吉田叶",   76, 78, 69, },
+    };
+    int      i;
+
+    for(i = 0; i < ELEM(students); i++)
+        Disp(students[i]);
+
+    return 0;
 }
